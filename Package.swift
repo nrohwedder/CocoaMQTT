c9640845b95eb87b1d2988fc0aca42b7a10e93a7
Nils Rohwedder <nrohwedder@gmx.de>
2020-07-02 14:17:38 +0200

    adding Package.swift for SPM support

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CocoaMQTT",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10)
+    ],
+    products: [
+        .library(name: "CocoaMQTT", targets: [ "CocoaMQTT" ]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket", from: "7.6.4"),
+    ],
+    targets: [
+        .target(name: "CocoaMQTT",
+                dependencies: [ "CocoaAsyncSocket" ],
+                path: "Source",
+                swiftSettings: [ .define("IS_SWIFT_PACKAGE")]),
+        .testTarget(name: "CocoaMQTTTests",
+                    dependencies: [ "CocoaMQTT" ],
+                    path: "CocoaMQTTTests",
+                    swiftSettings: [ .define("IS_SWIFT_PACKAGE")])
+    ]
+)
