0b6e489711d145ae62b2c0119ea2d5b82a1dcd42
Nils Rohwedder <nrohwedder@gmx.de>
2020-11-19 14:34:58 +0100

    adapting CocoaAsyncSocket dependency to use exact version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .library(name: "CocoaMQTT", targets: [ "CocoaMQTT" ]),
     ],
     dependencies: [
-        .package(url: "https://github.com/nrohwedder/CocoaAsyncSocket", .branch("release/7_6_5_SPM")),
+        .package(url: "https://github.com/nrohwedder/CocoaAsyncSocket", .exact("7.6.5")),
     ],
     targets: [
         .target(name: "CocoaMQTT",
